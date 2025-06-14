616aa8e9457430f46d86755c80bc53f57c59ebd5
zain0313233 <zain0313233@gmail.com>
2025-06-14 23:18:20 +0500

    frist comit

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express =require('express');
+const app=express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api/health',(req,res)=>{
+    res.status(200).json({
+        status:"ok",
+        message:"Server is Running"
+    })
+})
+const PORT = process.env.PORT || 3001;
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
+module.exports = app;
