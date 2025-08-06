60d58140e74c0c7250b27db433fb99bed2132fcf
zain0313233 <zain0313233@gmail.com>
2025-08-06 23:24:33 +0500

    implementing real time chat in ecommerce web app

--- socket/socketServer.js
+++ socket/socketServer.js
@@ -9,10 +9,46 @@ function initializeSocket(server) {
     }
   });
 
+  const connectedUsers = new Map();
+
   io.on('connection', (socket) => {
     console.log('User connected:', socket.id);
 
+    socket.on('join_chat', (data) => {
+      const { userId, userType } = data;
+      const roomId = `chat_${userId}`;
+      
+      socket.join(roomId);
+      socket.userId = userId;
+      socket.userType = userType;
+      socket.roomId = roomId;
+      
+      connectedUsers.set(socket.id, {
+        userId,
+        userType,
+        roomId,
+        socketId: socket.id
+      });
+
+      console.log(`${userType} ${userId} joined room: ${roomId}`);
+      
+      socket.to(roomId).emit('user_joined', {
+        userId,
+        userType,
+        message: `${userType} joined the chat`
+      });
+    });
+
     socket.on('disconnect', () => {
+      const userInfo = connectedUsers.get(socket.id);
+      if (userInfo) {
+        socket.to(userInfo.roomId).emit('user_left', {
+          userId: userInfo.userId,
+          userType: userInfo.userType,
+          message: `${userInfo.userType} left the chat`
+        });
+        connectedUsers.delete(socket.id);
+      }
       console.log('User disconnected:', socket.id);
     });
   });
